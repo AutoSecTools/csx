2b348888865ef1786af8dd27829011529bfca208
John Leitch <JohnLeitch@outlook.com>
2016-09-17 23:54:04 -0700

    Initial commit of C# extension.

---
+++ csx/Program.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csx
+{
+    public class TestRewriter : CSharpSyntaxRewriter
+    {
+        private bool HasNotifyAttribute(PropertyDeclarationSyntax node)
+        {
+            return node.AttributeLists
+                .Any(x => x.Attributes
+                    .Any(y => y.Name.ToString() == "NotifyPropertyChanged"));
+        }
+
+        private string GetAutoField(string propertyName)
+        {
+            return string.Format("__csx__{0}", propertyName);
+        }
+
+        static AccessorDeclarationSyntax GetSet(PropertyDeclarationSyntax node)
+        {
+            return node.AccessorList.Accessors
+                .SingleOrDefault(x => x.Keyword.Text == "set");
+        }
+
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            var children = node.Members;
+
+            var notifyProps = children
+                .OfType<PropertyDeclarationSyntax>()
+                .Where(HasNotifyAttribute)
+                .ToArray();
+
+            if (!notifyProps.Any())
+            {
+                return base.VisitClassDeclaration(node);
+            }
+
+            var autoFields = notifyProps
+                .Where(x => GetSet(x) != null && GetSet(x).Body == null)
+                .Select(x => new
+                {
+                    Property = x,
+                    Field = SyntaxFactory.FieldDeclaration(
+                        SyntaxFactory.VariableDeclaration(
+                            x.Type,
+                            SyntaxFactory.SeparatedList(
+                                new[]
+                                {
+                                    SyntaxFactory.VariableDeclarator(
+                                        GetAutoField(x.Identifier.Text))
+                                }))).WithTrailingTrivia(SyntaxFactory.Whitespace("\r\n"))
+                })
+                .ToArray();
+            
+            foreach (var f in autoFields)
+            {
+                var members = node.Members.Insert(
+                    node.Members.IndexOf(f.Property),
+                    f.Field.WithLeadingTrivia(SyntaxFactory.Whitespace("\r\n        ")));
+
+                node = node.WithMembers(members);
+            }
+
+            if (children
+                .OfType<MethodDeclarationSyntax>()
+                .Any(x => x.Identifier.ToString() == "InvokePropertyChanged"))
+            {
+                return base.VisitClassDeclaration(node);
+            }
+
+            node = node.AddMembers(GetInvokePropertyChanged());
+
+            return base.VisitClassDeclaration(node);
+        }
+
+        public override SyntaxNode VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+        {
+            if (!HasNotifyAttribute(node))
+            {
+                return base.VisitPropertyDeclaration(node);
+            }
+
+            var setter = GetSet(node);
+
+            if (setter == null || setter.Body == null)
+            {
+                return base.VisitPropertyDeclaration(node);
+            }
+            
+            var call = SyntaxFactory.ExpressionStatement(
+                SyntaxFactory.InvocationExpression(
+                    SyntaxFactory.IdentifierName("InvokePropertyChanged")));
+
+            node = node.WithAccessorList(
+                node.AccessorList.WithAccessors(
+                    node.AccessorList.Accessors
+                        .Remove(setter)
+                        .Add(setter.AddBodyStatements(call))));
+
+            return base.VisitPropertyDeclaration(node);
+        }
+
+        private MethodDeclarationSyntax GetInvokePropertyChanged()
+        {
+            var code = CSharpSyntaxTree.ParseText(
+@"
+        private void InvokePropertyChanged([CallerMemberName]string name = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+");
+
+            return code
+                .GetRoot()
+                .ChildNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Single();
+        }
+    }
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //CSharpSyntaxRewriter
+            var sw = new Stopwatch();
+            sw.Start();
+            var file = @"..\..\TestViewModel.cs";
+
+            var ast = CSharpSyntaxTree.ParseText(File.ReadAllText(file));
+            var astCode = ast.ToString();
+            var root = new TestRewriter().Visit(ast.GetRoot());//.NormalizeWhitespace();
+            var output = root.ToString();
+            
+            sw.Stop();
+            Console.WriteLine("Done in {0}ms", sw.ElapsedMilliseconds);
+            return;
+            var descendants = ast
+                .GetCompilationUnitRoot()
+                .DescendantNodes()
+                .OfType<PropertyDeclarationSyntax>()
+                //.Where(x => x.
+                .ToArray();
+        }
+    }
+}
---
+++ csx/TestViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csx
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NotifyPropertyChangedAttribute : Attribute
+    {
+    }
+
+    public class TestViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double NoNotify { get; set; }
+
+        private double _x;
+
+        [NotifyPropertyChanged]
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        [NotifyPropertyChanged]
+        public double Y { get; set; }
+
+        private double _z;
+
+        [NotifyPropertyChanged]
+        public double Z
+        {
+            get { return _z; }
+        }
+
+        
+    }
+}
